ba564cdf3ef946c790e27fd2a34c6f86f2c356cc
Ray Donnelly <mingw.android@gmail.com>
2021-02-04 09:27:44 +0100

    another fix for installing things

--- recipe/install-libgcc-devel.sh
+++ recipe/install-libgcc-devel.sh
@@ -1,6 +1,8 @@
 set -e -x
 
 CHOST=$(${SRC_DIR}/.build/*-*-*-*/build/build-cc-gcc-final/gcc/xgcc -dumpmachine)
+declare -a COMMON_MAKE_OPTS=()
+COMMON_MAKE_OPTS+=(prefix=${PREFIX} exec_prefix=${PREFIX})
 
 # libtool wants to use ranlib that is here, macOS install doesn't grok -t etc
 # .. do we need this scoped over the whole file though?
@@ -8,7 +10,7 @@ export PATH=${SRC_DIR}/gcc_built/bin:${SRC_DIR}/.build/${CHOST}/buildtools/bin:$
 
 pushd ${SRC_DIR}/.build/${CHOST}/build/build-cc-gcc-final/
 
-make -C ${CHOST}/libgcc prefix=${PREFIX} install
+make -C ${CHOST}/libgcc "${COMMON_MAKE_OPTS[@]}" install
 
 # These go into libgcc output
 rm -rf ${PREFIX}/${CHOST}/lib
