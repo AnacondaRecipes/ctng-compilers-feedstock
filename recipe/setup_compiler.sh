8765b7c5fcc5780c7ac59cd2408f95f1b835ff43
Ian Fitchet <ifitchet@anaconda.com>
2025-09-19 17:55:23 +0100

    remove CF channels

--- recipe/setup_compiler.sh
+++ recipe/setup_compiler.sh
@@ -31,8 +31,7 @@ if [[ ! -d  $SRC_DIR/cf-compilers ]]; then
     )
     # Remove conda-forge/label/sysroot-with-crypt when GCC < 14 is dropped
     #conda create -p $SRC_DIR/cf-compilers --yes --quiet \
-    conda create -p $SRC_DIR/cf-compilers -c conda-forge/label/sysroot-with-crypt -c conda-forge --yes --quiet \
-	-c local \
+    conda create -p $SRC_DIR/cf-compilers --yes --quiet \
       "binutils_impl_${build_platform}" \
       "gcc_impl_${build_platform}" \
       "gxx_impl_${build_platform}" \
