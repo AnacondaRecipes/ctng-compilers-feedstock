7e8b9edebcc76c4015e54101a1c87d5b3f1d6297
Eric Lundby <Eric.Lundby@gmail.com>
2026-05-18 13:30:49 -0600

    Update setup_compiler.sh

--- recipe/setup_compiler.sh
+++ recipe/setup_compiler.sh
@@ -14,7 +14,6 @@ if [[ ! -d  $SRC_DIR/cf-compilers ]]; then
         "gfortran_impl_${cross_target_platform}=${gcc_version}"
       )
     fi
-    conda config --set plugins.use_sharded_repodata false
     # Remove conda-forge/label/sysroot-with-crypt when GCC < 14 is dropped
     conda create -p $SRC_DIR/cf-compilers --yes --quiet \
       "binutils_impl_${build_platform}" \
