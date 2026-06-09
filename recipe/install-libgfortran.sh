a95dcfb353403a5bcebdefd05d8f31f6dc467c79
Jean-Christophe Morin <jcmorin@anaconda.com>
2026-06-09 18:35:07 -0400

    Apply patches

--- recipe/install-libgfortran.sh
+++ recipe/install-libgfortran.sh
@@ -16,3 +16,23 @@ fi
 # Install Runtime Library Exception
 install -Dm644 $SRC_DIR/COPYING.RUNTIME \
         ${PREFIX}/share/licenses/libgfortran/RUNTIME.LIBRARY.EXCEPTION
+
+# https://git.almalinux.org/rpms/gcc-toolset-15-gcc/src/commit/4d45dd5368467d758b31cda493a5fb92080746fd/gcc-toolset-15-gcc.spec#L390
+cp -v -a ${SRC_DIR}/build/${TARGET}/libgfortran/.libs/libgfortran_nonshared80.a \
+  ${PREFIX}/lib/libgfortran_nonshared.a
+
+if [[ "$target_platform" == 'linux-64' ]]; then
+  oformat='OUTPUT_FORMAT(elf64-x86-64)'
+elif [[ "$target_platform" == 'linux-aarch64' ]]; then
+  oformat='OUTPUT_FORMAT(elf64-littleaarch64)'
+else
+  echo "Unknown platform"
+  exit 1
+fi
+
+rm -f ${PREFIX}/lib/libgfortran.so
+echo "/* GNU ld script
+   Use the shared library, but some functions are only in
+   the static library, so try that secondarily.  */
+${oformat}
+INPUT ( ${PREFIX}/lib/libgfortran.so.5 -lgfortran_nonshared )" > ${PREFIX}/lib/libgfortran.so
--- recipe/install-libstdc++.sh
+++ recipe/install-libstdc++.sh
@@ -20,10 +20,32 @@ mkdir -p ${PREFIX}/lib
 #mv ${PREFIX}/${CHOST}/lib/* ${PREFIX}/lib
 
 # no static libs
-find ${PREFIX}/lib -name "*\.a" -exec rm -rf {} \;
+find ${PREFIX}/lib -name "*\.a" -exec rm -rf {} -v \;
 # no libtool files
-find ${PREFIX}/lib -name "*\.la" -exec rm -rf {} \;
+find ${PREFIX}/lib -name "*\.la" -exec rm -rf {} -v \;
 
 # Install Runtime Library Exception
 install -Dm644 ${SRC_DIR}/COPYING.RUNTIME \
         ${PREFIX}/share/licenses/libstdc++/RUNTIME.LIBRARY.EXCEPTION
+
+# https://git.almalinux.org/rpms/gcc-toolset-15-gcc/src/commit/4d45dd5368467d758b31cda493a5fb92080746fd/gcc-toolset-15-gcc.spec#L390
+cp -v -a ${SRC_DIR}/build/${TARGET}/libstdc++-v3/src/.libs/libstdc++_nonshared80.a \
+  ${PREFIX}/lib/libstdc++_nonshared.a
+
+if [[ "$target_platform" == 'linux-64' ]]; then
+  oformat='OUTPUT_FORMAT(elf64-x86-64)'
+elif [[ "$target_platform" == 'linux-aarch64' ]]; then
+  oformat='OUTPUT_FORMAT(elf64-littleaarch64)'
+else
+  echo "Unknown platform"
+  exit 1
+fi
+
+libstdcxx_so="${PREFIX}/lib/libstdc++.so.6"
+libstdcxx_so_link="INPUT ( ${libstdcxx_so} -lstdc++_nonshared AS_NEEDED (${libstdcxx_so}) )"
+
+echo "/* GNU ld script
+   Use the shared library, but some functions are only in
+   the static library, so try that secondarily.  */
+${oformat}
+${libstdcxx_so_link}" > ${PREFIX}/lib/libstdc++.so
