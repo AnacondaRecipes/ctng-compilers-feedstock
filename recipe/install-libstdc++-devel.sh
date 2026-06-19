9520af741e4821839f06f1c4dbe17981bf7013a7
Jean-Christophe Morin <jcmorin@anaconda.com>
2026-06-19 17:43:31 -0400

    Move libstdc++ linker script into the tripplet lib folder inside the libstdcxx-devel package

--- recipe/install-libstdc++-devel.sh
+++ recipe/install-libstdc++-devel.sh
@@ -31,3 +31,25 @@ fi
 
 popd
 
+# https://git.almalinux.org/rpms/gcc-toolset-15-gcc/src/commit/4d45dd5368467d758b31cda493a5fb92080746fd/gcc-toolset-15-gcc.spec#L390
+cp -v -a ${SRC_DIR}/build/${TARGET}/libstdc++-v3/src/.libs/libstdc++_nonshared80.a \
+  ${PREFIX}/lib/gcc/${CHOST}/${gcc_version}/libstdc++_nonshared.a
+
+if [[ "$target_platform" == 'linux-64' ]]; then
+  oformat='OUTPUT_FORMAT(elf64-x86-64)'
+elif [[ "$target_platform" == 'linux-aarch64' ]]; then
+  oformat='OUTPUT_FORMAT(elf64-littleaarch64)'
+else
+  echo "Unknown platform"
+  exit 1
+fi
+
+libstdcxx_so="${PREFIX}/lib/libstdc++.so.6"
+libstdcxx_so_link="INPUT ( ${libstdcxx_so} -lstdc++_nonshared AS_NEEDED (${libstdcxx_so}) )"
+
+rm -v -f ${PREFIX}/lib/gcc/${CHOST}/${gcc_version}/libstdc++.so
+echo "/* GNU ld script
+   Use the shared library, but some functions are only in
+   the static library, so try that secondarily.  */
+${oformat}
+${libstdcxx_so_link}" > ${PREFIX}/lib/gcc/${CHOST}/${gcc_version}/libstdc++.so
