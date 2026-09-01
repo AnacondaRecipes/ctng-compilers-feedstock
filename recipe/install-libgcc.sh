d98c97fb10754fa3cf1df3c15ad3eccd058926a1
Jean-Christophe Morin <jcmorin@anaconda.com>
2026-09-01 18:15:50 -0400

    Remove name prefix. We will instead use variants to simplify things and avoid unnecessary complicts while doing rebuilds

--- recipe/install-libgcc.sh
+++ recipe/install-libgcc.sh
@@ -9,12 +9,12 @@ set -e -x
 
 pushd ${SRC_DIR}/build
 
-  if [[ "${PKG_NAME}" == "${PACKAGE_NAME_PREFIX}libgcc" ]]; then
+  if [[ "${PKG_NAME}" == "libgcc" ]]; then
     make -C ${TARGET}/libgcc prefix=${PREFIX} install-shared
     if [[ "${TARGET}" == *mingw* ]]; then
       mv $PREFIX/lib/libgcc_s*.dll $PREFIX/bin
     fi
-  elif [[ "${PKG_NAME}" != "${PACKAGE_NAME_PREFIX}gcc_impl"* ]]; then
+  elif [[ "${PKG_NAME}" != "gcc_impl"* ]]; then
     # when building a cross compiler, above make line will clobber $PREFIX/lib/libgcc_s.so.1
     # and fail after some point for some architectures. To avoid that, we copy manually
     pushd ${TARGET}/libgcc
@@ -61,14 +61,14 @@ popd
 
 mkdir -p ${PREFIX}/lib
 
-if [[ "${PKG_NAME}" != "${PACKAGE_NAME_PREFIX}gcc_impl"* ]]; then
+if [[ "${PKG_NAME}" != "gcc_impl"* ]]; then
   # no static libs
   find ${PREFIX}/lib -name "*\.a" -exec rm -v -rf {} \;
 fi
 # no libtool files
 find ${PREFIX}/lib -name "*\.la" -exec rm -v rf {} \;
 
-if [[ "${PKG_NAME}" != ${PACKAGE_NAME_PREFIX}gcc_impl* ]]; then
+if [[ "${PKG_NAME}" != gcc_impl* ]]; then
   # mv ${PREFIX}/${TARGET}/lib/* ${PREFIX}/lib
   # clean up empty folder
   rm -rf ${PREFIX}/lib/gcc
--- recipe/install-symlinks.sh
+++ recipe/install-symlinks.sh
@@ -8,13 +8,13 @@ else
   symlink_or_copy="ln -sf"
 fi
 
-if [[ "${PKG_NAME}" == "${PACKAGE_NAME_PREFIX}gcc" ]]; then
+if [[ "${PKG_NAME}" == "gcc" ]]; then
   for tool in cc cpp gcc gcc-ar gcc-nm gcc-ranlib gcov gcov-dump gcov-tool; do
     $symlink_or_copy ${PREFIX}/bin/${triplet}-${tool}${EXEEXT} ${PREFIX}/bin/${tool}${EXEEXT}
   done
-elif [[ "${PKG_NAME}" == "${PACKAGE_NAME_PREFIX}gxx" ]]; then
+elif [[ "${PKG_NAME}" == "gxx" ]]; then
   $symlink_or_copy ${PREFIX}/bin/${triplet}-g++${EXEEXT} ${PREFIX}/bin/g++${EXEEXT}
   $symlink_or_copy ${PREFIX}/bin/${triplet}-c++${EXEEXT} ${PREFIX}/bin/c++${EXEEXT}
-elif [[ "${PKG_NAME}" == "${PACKAGE_NAME_PREFIX}gfortran" ]]; then
+elif [[ "${PKG_NAME}" == "gfortran" ]]; then
   $symlink_or_copy ${PREFIX}/bin/${triplet}-gfortran${EXEEXT} ${PREFIX}/bin/gfortran${EXEEXT}
 fi
